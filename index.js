d7a53caa6049a40de3b00dab13f84633726b8ea2
Ayush <ayushbisht750083@gmail.com>
2023-08-21 02:55:46 +0530

    from

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+
+function handleclick(event){
+
+
+
+
+
+
+
+
+event.preventDefault;
+
+  alert("thankyou")}
+
+
+
+
+
+   function handlechange(event,id){
+   event.preventDefault;
+//    console.log(id);
+
+   if(id=='name'){
+     var naam=event;
+    console.log(event);  }
+    else if(id=='password'){
+        console.log("password");
+        if(event==naam||event.length<4){
+            alert("weak password");
+        }
+       console.log(event);
+       var someVarName = event;
+localStorage.setItem("someVarKey", someVarName);
+        const pass=event;
+        console.log(pass);
+
+    }else if(id=='newpassword'){
+        console.log("newpassword");
+        var someVarName = localStorage.getItem("someVarKey");
+        if(event!=someVarName){
+            console.log("else");
+            console.log(event);
+            console.log(someVarName);
+            alert("your password does not match");
+        }
+    }
+
+
+
+
+}
